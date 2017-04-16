bdcccb54667d8ca506d7ad18d92317912866cc16
James Mitchell <mitchell.thehumantornado@gmail.com>
2017-04-16 15:26:32 -0500

    removed previous project files

--- server/routes.js
+++
@@ -1,6 +0,0 @@
-var handler = require('./request-handler.js');
-
-module.exports = function(app, express) {
-  app.get('/api/borrowcats', handler.listCats);
-  app.post('api/borrowcats', handler.addCat)
-}
